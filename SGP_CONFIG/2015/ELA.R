e0086af77050aae11b98337e22cf8bf49059e93d
dbetebenner <dbetebenner@gmail.com>
2016-09-18 03:56:29 -0400

    Update of ELA configuration

--- SGP_CONFIG/2015/ELA.R
+++ SGP_CONFIG/2015/ELA.R
@@ -5,7 +5,7 @@
 ################################################################################
 
 ###  Use BOTH TCAP Writing and Reading as priors for PARCC ELA
-##    Unlike other End-of-Grade subjects, use sgp.exact.grade.progression 
+##    Unlike other End-of-Grade subjects, use sgp.exact.grade.progression
 ##     and sgp.norm.group.preference for ELA with double priors
 
 ELA_2015.config <- list(
@@ -14,16 +14,16 @@ ELA_2015.config <- list(
 		sgp.panel.years=c("2014", "2014", "2015"),
 		sgp.grade.sequences=list(c("3", "3", "4"), c("4", "4", "5"), c("5", "5", "6"),
 			c("6", "6", "7"), c("7", "7", "8"), c("8", "8", "9")),
-		sgp.exact.grade.progression=list(list(TRUE), 6),
+		sgp.exact.grade.progression=rep(list(TRUE), 6),
 		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 6)),
 		sgp.norm.group.preference=3),
 
 	ELA.2015 = list( # 2 Years of priors
 		sgp.content.areas=c("READING", "WRITING", "READING", "WRITING", "ELA"),
 		sgp.panel.years=c("2013", "2013", "2014", "2014", "2015"),
-		sgp.grade.sequences=list(c("3", "3", "4", "4", "5"), c("4", "4", "5", "5", "6"), c("5", "5", "6", "6", "7"), 
+		sgp.grade.sequences=list(c("3", "3", "4", "4", "5"), c("4", "4", "5", "5", "6"), c("5", "5", "6", "6", "7"),
 			c("6", "6", "7", "7", "8"), c("7", "7", "8", "8", "9")),
-		sgp.exact.grade.progression=list(list(TRUE), 5),
+		sgp.exact.grade.progression=rep(list(TRUE), 5),
 		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 5)),
 		sgp.norm.group.preference=2),
 
@@ -32,7 +32,7 @@ ELA_2015.config <- list(
 		sgp.panel.years=c("2012", "2012", "2013", "2013", "2014", "2014", "2015"),
 		sgp.grade.sequences=list(c("3", "3", "4", "4", "5", "5", "6"), c("4", "4", "5", "5", "6", "6", "7"), c("5", "5", "6", "6", "7", "7", "8"),
 			c("6", "6", "7", "7", "8", "8", "9")),
-		sgp.exact.grade.progression=list(list(TRUE), 4),
+		sgp.exact.grade.progression=rep(list(TRUE), 4),
 		sgp.projection.grade.sequences=list(rep("NO_PROJECTIONS", 4)),
 		sgp.norm.group.preference=1)
 
